768f715e2ad6d488a79248e10ff4bf196e099811
gemalors <monserratloor@yahoo.com>
2020-02-08 15:58:15 -0500

    Actualizamos el merge

--- resources/views/vendor/adminlte/categoria.blade.php
+++ resources/views/vendor/adminlte/categoria.blade.php
@@ -9,40 +9,24 @@
 	<div class="container-fluid spark-screen">
 	<div class="box box-info">
             <div class="box-header with-border">
-              <h3 class="box-title">Horizontal Form</h3>
+              <h3 class="box-title">Registro de Categorias</h3>
             </div>
             <!-- /.box-header -->
             <!-- form start -->
             <form class="form-horizontal">
               <div class="box-body">
                 <div class="form-group">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                  <label for="inputEmail3" class="col-sm-2 control-label">Nombre</label>
 
                   <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-
-                  <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="col-sm-offset-2 col-sm-10">
-                    <div class="checkbox">
-                      <label>
-                        <input type="checkbox"> Remember me
-                      </label>
-                    </div>
+                    <input type="text" class="form-control" id="nombrecategoria" placeholder="categoria">
                   </div>
                 </div>
               </div>
               <!-- /.box-body -->
               <div class="box-footer">
-                <button type="submit" class="btn btn-default">Cancel</button>
-                <button type="submit" class="btn btn-info pull-right">Sign in</button>
+                <button type="submit" class="btn btn-default">Cancelar</button>
+                <button type="submit" class="btn btn-info pull-right">Registrar</button>
               </div>
               <!-- /.box-footer -->
             </form>
